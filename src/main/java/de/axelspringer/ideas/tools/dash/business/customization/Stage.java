077c27ae2f5e444c70cb47ee29c390c2082fc2a2
swaschni <swaschni@XIN851573.asv.local>
2015-03-20 19:36:55 +0100

    Remove PCP-Stuff #1

--- src/main/java/de/axelspringer/ideas/tools/dash/business/customization/Stage.java
+++ src/main/java/de/axelspringer/ideas/tools/dash/business/customization/Stage.java
@@ -3,8 +3,10 @@ package de.axelspringer.ideas.tools.dash.business.customization;
 public interface Stage {
     String getStageId();
 
+    // FIXME move to PcpStage
     String getLoadbalancerHost();
 
+    // FIXME move to PcpStage
     String getLoadbalancerHostWs();
 
     String getJiraName();
