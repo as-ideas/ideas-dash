cf819a68ec166a91ba28d60fbc82c20142e6283c
gaffa <igaffai@gmail.com>
2015-04-30 11:40:00 +0200

    less cpu-intense "in progress" visualization

--- dash-example/src/main/java/de/axelspringer/ideas/tools/dash/example/ExampleCheckExecutor.java
+++ dash-example/src/main/java/de/axelspringer/ideas/tools/dash/example/ExampleCheckExecutor.java
@@ -6,15 +6,21 @@ import de.axelspringer.ideas.tools.dash.business.check.CheckResult;
 import de.axelspringer.ideas.tools.dash.presentation.State;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class ExampleCheckExecutor implements CheckExecutor<ExampleCheck> {
 
     @Override
     public List<CheckResult> executeCheck(ExampleCheck check) {
-        return Collections.singletonList(new CheckResult(State.YELLOW, check.getName(), "info", 0, 1, check.getGroup()));
+
+        final CheckResult result = new CheckResult(State.YELLOW, check.getName(), "info", 0, 1, check.getGroup());
+        if (new Random().nextInt() % 2 == 0) {
+            result.markRunning();
+        }
+        return Arrays.asList(result, result, result);
     }
 
     @Override
