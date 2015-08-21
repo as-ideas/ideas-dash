e6137e233b95660f299796e6d1ba3c1e24868138
Timo Ulich <timo.ulich@asideas.de>
2015-08-21 15:27:37 +0200

    add some output to debug

--- travis-build.sh
+++ travis-build.sh
@@ -1,8 +1,11 @@
 # only deploy a new version to the maven repo from the master branch
+echo "building branch $TRAVIS_BRANCH (pullRequest: $TRAVIS_PULL_REQUEST)"
+
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     goal=deploy
 else
     goal=install
 fi
 
+echo "mvn ${goal}"
 mvn ${goal} --settings settings.xml
