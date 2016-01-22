1d51506ae9410f0e8b204719a1cbfd9ede62642f
Fernandez Pajares <maria@upday.com>
2016-01-22 17:18:08 +0100

    [POC-57] cleaned imports

--- dash-core/src/main/java/de/axelspringer/ideas/tools/dash/business/check/CheckService.java
+++ dash-core/src/main/java/de/axelspringer/ideas/tools/dash/business/check/CheckService.java
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import de.axelspringer.ideas.tools.dash.business.customization.Team;
 import de.axelspringer.ideas.tools.dash.presentation.State;
 
 
