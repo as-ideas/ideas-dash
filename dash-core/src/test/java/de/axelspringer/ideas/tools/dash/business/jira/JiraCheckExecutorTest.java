0a4a4b8836efa4b53b471c00b085f7162eb7ddf7
gaffa <igaffai@gmail.com>
2016-03-21 15:40:15 +0100

    fix tests

--- dash-core/src/test/java/de/axelspringer/ideas/tools/dash/business/jira/JiraCheckExecutorTest.java
+++ dash-core/src/test/java/de/axelspringer/ideas/tools/dash/business/jira/JiraCheckExecutorTest.java
@@ -9,16 +9,19 @@ import de.axelspringer.ideas.tools.dash.presentation.State;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
@@ -33,15 +36,18 @@ public class JiraCheckExecutorTest {
     public static final String GIVEN_JQL = "jql";
     public static final Group GIVEN_GROUP = mock(Group.class);
 
+    @Mock
+    private JiraClient jiraClient;
+
     @Spy
     @InjectMocks
     private JiraCheckExecutor jiraCheckExecutor;
 
     @Test
     public void executeChecks_WithNoIssuesFound() throws Exception {
-        doReturn(new SearchResult()).when(jiraCheckExecutor).queryJira(any(JiraCheck.class));
+        doReturn(new SearchResult().getIssues()).when(jiraClient).queryJiraForIssues(anyString(), anyString(), anyString(), anyString());
 
-        List<CheckResult> checkResults = jiraCheckExecutor.executeCheck(new JiraCheck(GIVEN_NAME, Arrays.asList(GIVEN_TEAM), GIVEN_URL, GIVEN_USER, GIVEN_PASSWORT, GIVEN_JQL, GIVEN_GROUP));
+        List<CheckResult> checkResults = jiraCheckExecutor.executeCheck(new JiraCheck(GIVEN_NAME, Collections.singletonList(GIVEN_TEAM), GIVEN_URL, GIVEN_USER, GIVEN_PASSWORT, GIVEN_JQL, GIVEN_GROUP));
 
         assertThat(checkResults.size(), is(1));
         CheckResult singleResult = checkResults.get(0);
@@ -60,7 +66,7 @@ public class JiraCheckExecutorTest {
         givenTwoIssuesAreFound();
         givenDoNothingForFoundIssues();
 
-        List<CheckResult> checkResults = jiraCheckExecutor.executeCheck(new JiraCheck(GIVEN_NAME, Arrays.asList(GIVEN_TEAM), GIVEN_URL, GIVEN_USER, GIVEN_PASSWORT, GIVEN_JQL, GIVEN_GROUP));
+        List<CheckResult> checkResults = jiraCheckExecutor.executeCheck(new JiraCheck(GIVEN_NAME, Collections.singletonList(GIVEN_TEAM), GIVEN_URL, GIVEN_USER, GIVEN_PASSWORT, GIVEN_JQL, GIVEN_GROUP));
 
         assertThat(checkResults.size(), is(2));
     }
@@ -68,7 +74,7 @@ public class JiraCheckExecutorTest {
     private void givenTwoIssuesAreFound() {
         SearchResult toBeReturned = new SearchResult();
         toBeReturned.setIssues(Arrays.asList(new Issue(), new Issue()));
-        doReturn(toBeReturned).when(jiraCheckExecutor).queryJira(any(JiraCheck.class));
+        doReturn(toBeReturned.getIssues()).when(jiraClient).queryJiraForIssues(anyString(), anyString(), anyString(), anyString());
     }
 
     private void givenDoNothingForFoundIssues() {
