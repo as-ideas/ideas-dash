cc700aff1137f6d6175df42c0164eda29344941b
gaffa <igaffai@gmail.com>
2016-09-04 12:20:42 +0200

    add unique identifier for checkresults to later be able to map comments to

--- dash-core/src/main/java/de/axelspringer/ideas/tools/dash/business/jenkins/executor/JenkinsPipelineExecutor.java
+++ dash-core/src/main/java/de/axelspringer/ideas/tools/dash/business/jenkins/executor/JenkinsPipelineExecutor.java
@@ -78,7 +78,8 @@ public class JenkinsPipelineExecutor {
                                             .withOrder(checkIndex)
                                             .withLink(lastBuild.getUrl())
                                             .withTeams(check.getTeams())
-                                            .withIconSrc("http://www.kidsmathgamesonline.com/images/pictures/numbers120/number" + checkIndex + ".jpg");
+                                            .withIconSrc("http://www.kidsmathgamesonline.com/images/pictures/numbers120/number" + checkIndex + ".jpg")
+                                            .withCheckResultIdentifier(lastBuild.getUrl() + "_(stage" + checkIndex + ")");
                                 }
                         )
                         .collect(Collectors.toList()));
