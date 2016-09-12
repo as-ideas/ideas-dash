2786e624bc8b69803187cce5f7ae99a8d8d04cdf
Timo Ulich <timo@ulich.net>
2016-09-12 14:21:18 +0200

    make example dash work with comments

--- dash-example/src/main/java/de/axelspringer/ideas/tools/dash/example/ExampleGroups.java
+++ dash-example/src/main/java/de/axelspringer/ideas/tools/dash/example/ExampleGroups.java
@@ -7,12 +7,12 @@ import java.util.List;
 
 public class ExampleGroups {
 
-    public final static Group SIXTH = new Group(false, 6, null, "SIXTH");
-    public final static Group FOURTH = new Group(false, 4, null, "FOURTH");
-    public final static Group FIFTH = new Group(false, 5, null, "FIFTH");
-    public final static Group FIRST = new Group(false, 1, null, "FIRST");
-    public final static Group THIRD = new Group(false, 3, null, "THIRD");
-    public final static Group SECOND = new Group(false, 2, null, "SECOND");
+    public final static Group SIXTH = new Group(false, 6, "team-6", "SIXTH");
+    public final static Group FOURTH = new Group(false, 4, "team-4", "FOURTH");
+    public final static Group FIFTH = new Group(false, 5, "team-5", "FIFTH");
+    public final static Group FIRST = new Group(false, 1, "team-1", "FIRST");
+    public final static Group THIRD = new Group(false, 3, "team-3", "THIRD");
+    public final static Group SECOND = new Group(false, 2, "team-2", "SECOND");
 
     public static List<Group> exampleGroups() {
         return Arrays.asList(SIXTH, FOURTH, FIFTH, FIRST, THIRD, SECOND);
