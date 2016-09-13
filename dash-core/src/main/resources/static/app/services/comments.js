dcb414df12424a34afd3a33d7f341d1c2e62c547
gaffa <igaffai@gmail.com>
2016-09-13 17:16:17 +0200

    formatting...

--- dash-core/src/main/resources/static/app/services/comments.js
+++ dash-core/src/main/resources/static/app/services/comments.js
@@ -7,27 +7,27 @@ angular.module('dashapp')
             // backend-communication
             var commentResource = $resource('rest/comments');
 
-        var syncComments = function () {
+            var syncComments = function () {
 
-            // load comments from local storage/persistence
-            var savedComments = Persistence.load('comments', []);
+                // load comments from local storage/persistence
+                var savedComments = Persistence.load('comments', []);
 
-            // post them to the server
-            commentResource.save(savedComments).$promise.then(function () {
-                // load from server
-                commentResource.query().$promise.then(function (serverComments) {
-                    // save to storage
-                    Persistence.save('comments', serverComments);
+                // post them to the server
+                commentResource.save(savedComments).$promise.then(function () {
+                    // load from server
+                    commentResource.query().$promise.then(function (serverComments) {
+                        // save to storage
+                        Persistence.save('comments', serverComments);
+                    });
                 });
-                });
-        };
+            };
 
             // public API
             var commentService = {};
 
-        commentService.comments = function () {
-            syncComments();
-            return Persistence.load('comments', []);
+            commentService.comments = function () {
+                syncComments();
+                return Persistence.load('comments', []);
             };
 
             commentService.comment = function (comment) {
