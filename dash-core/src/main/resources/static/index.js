464d073d2369ee744c0867146e0bce8b1de54f77
gaffa <igaffai@gmail.com>
2016-09-18 10:28:31 +0200

    remove csrf-config (did not work, will be solved server-side)

--- dash-core/src/main/resources/static/index.js
+++ dash-core/src/main/resources/static/index.js
@@ -1,8 +1,4 @@
 angular.module('dashapp', ['ngResource', 'ngSanitize'])
-    .config(['$httpProvider', function ($httpProvider) {
-        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-    }])
     .controller('indexcontroller', function ($scope, AppConfig) {
         // inject page title
         AppConfig.get().$promise.then(function (config) {
