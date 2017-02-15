95c8a0dd507d15da8194d817074e8cca64ab7490
gaffa <igaffai@gmail.com>
2017-02-15 11:10:00 +0100

    yellow appeared green <- fixed

--- dash-core/src/main/resources/static/app/services/philipshue.js
+++ dash-core/src/main/resources/static/app/services/philipshue.js
@@ -7,7 +7,7 @@ angular.module('dashapp')
             var hueService = {};
 
             hueService.GREEN = {"on": true, "hue": 25500, "sat": 254, "bri": 180};
-            hueService.YELLOW = {"on": true, "hue": 12750, "sat": 254, "bri": 254};
+        hueService.YELLOW = {"on": true, "hue": 9000, "sat": 254, "bri": 254};
             hueService.RED = {"on": true, "hue": 0, "sat": 254, "bri": 254};
             hueService.BLUE = {"on": true, "hue": 46000, "sat": 254, "bri": 254};
 
