e7f2fedfdf526e1279c16065bf49bc8b8372b80c
Simon Aquino <simon.aquino@digital.hmrc.gov.uk>
2017-06-20 20:38:52 +0200

    Don't explicity pass credentials to the AmazonCloudWatchClientBuilder
    The Client should implicitly retrieve the credentials using the default
    AWS chain.

--- dash-core/src/main/java/de/axelspringer/ideas/tools/dash/business/cloudwatch/CloudWatchService.java
+++ dash-core/src/main/java/de/axelspringer/ideas/tools/dash/business/cloudwatch/CloudWatchService.java
@@ -1,7 +1,5 @@
 package de.axelspringer.ideas.tools.dash.business.cloudwatch;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.cloudwatch.AmazonCloudWatchClientBuilder;
 import com.amazonaws.services.cloudwatch.model.DescribeAlarmsResult;
 import org.springframework.stereotype.Service;
@@ -11,11 +9,8 @@ public class CloudWatchService {
 
     public DescribeAlarmsResult describeAlarms(String awsRegion) {
 
-        final DefaultAWSCredentialsProviderChain credentialsProviderChain = new DefaultAWSCredentialsProviderChain();
-        final AWSStaticCredentialsProvider credentialProvider = new AWSStaticCredentialsProvider(credentialsProviderChain.getCredentials());
         return AmazonCloudWatchClientBuilder
                 .standard()
-                .withCredentials(credentialProvider)
                 .withRegion(awsRegion)
                 .build()
                 .describeAlarms();
