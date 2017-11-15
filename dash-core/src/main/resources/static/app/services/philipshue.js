8c2e36961eec6a394ff4c7dcec5e28dcb2258ac8
Timo Ulich <ulich@users.noreply.github.com>
2017-11-15 09:24:50 +0100

    support multiple lights (#103)

--- dash-core/src/main/resources/static/app/services/philipshue.js
+++ dash-core/src/main/resources/static/app/services/philipshue.js
@@ -7,25 +7,27 @@ angular.module('dashapp')
             var hueService = {};
 
             hueService.GREEN = {"on": true, "hue": 25500, "sat": 254, "bri": 180};
-        hueService.YELLOW = {"on": true, "hue": 9000, "sat": 254, "bri": 254};
+            hueService.YELLOW = {"on": true, "hue": 9000, "sat": 254, "bri": 254};
             hueService.RED = {"on": true, "hue": 0, "sat": 254, "bri": 254};
             hueService.BLUE = {"on": true, "hue": 46000, "sat": 254, "bri": 254};
 
             hueService.update = function (hueConfig, data) {
 
                 // check params
-                if (!hueConfig['ip'] || !hueConfig['key'] || !hueConfig['light'] || !data) {
+                if (!hueConfig.ip || !hueConfig.key || !hueConfig.light || !data) {
                     console.error("invalid parameters, cannot update hue");
                     return;
                 }
 
-                var hueResource = $resource("http://" + hueConfig['ip'] + "/api/" + hueConfig['key'] + "/lights/" + hueConfig['light'] + "/state", null, {
-                    'update': {
-                        method: 'PUT',
-                        isArray: true
-                    }
-                });
-                hueResource.update(data);
+                hueConfig.light.split(',').forEach(light => {
+                    var hueResource = $resource("http://" + hueConfig['ip'] + "/api/" + hueConfig['key'] + "/lights/" + light + "/state", null, {
+                        'update': {
+                            method: 'PUT',
+                            isArray: true
+                        }
+                    });
+                    hueResource.update(data);
+                })
             };
 
             return hueService;
