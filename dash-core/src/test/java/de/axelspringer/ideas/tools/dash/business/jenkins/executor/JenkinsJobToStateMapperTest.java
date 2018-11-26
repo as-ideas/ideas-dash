9da2f0897304c39462fc10aae709a86481b868d2
gaffa <igaffai@gmail.com>
2018-11-26 14:27:24 +0100

    fix tests, add tests...

--- dash-core/src/test/java/de/axelspringer/ideas/tools/dash/business/jenkins/executor/JenkinsJobToStateMapperTest.java
+++ dash-core/src/test/java/de/axelspringer/ideas/tools/dash/business/jenkins/executor/JenkinsJobToStateMapperTest.java
@@ -1,5 +1,6 @@
 package de.axelspringer.ideas.tools.dash.business.jenkins.executor;
 
+import de.axelspringer.ideas.tools.dash.business.jenkins.domain.Build;
 import de.axelspringer.ideas.tools.dash.business.jenkins.domain.JenkinsBuildInfo;
 import de.axelspringer.ideas.tools.dash.business.jenkins.domain.JenkinsJobInfo;
 import de.axelspringer.ideas.tools.dash.business.jenkins.domain.Property;
@@ -8,59 +9,59 @@ import org.junit.Test;
 
 import java.util.Collections;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class JenkinsJobToStateMapperTest {
 
     private JenkinsJobToStateMapper mapper = new JenkinsJobToStateMapper();
 
     @Test
-    public void decreaseSeverityMultiBranchMaster() throws Exception {
-
-        assertFalse(mapper.decreaseSeverity(jobInfo(true, true)));
+    public void identifyStatusFailedTestCountRegular() throws Exception {
+        assertEquals(State.YELLOW, mapper.identifyStatus(buildInfo(), 1, jobInfo(false, true, build(false))));
     }
 
     @Test
-    public void decreaseSeverityMultiBranchFeatureBranch() throws Exception {
-
-        assertTrue(mapper.decreaseSeverity(jobInfo(true, false)));
+    public void identifyStatusFailedTestCountActiveBranch() throws Exception {
+        assertEquals(State.GREEN, mapper.identifyStatus(buildInfo(), 1, jobInfo(true, false, build(false))));
     }
 
     @Test
-    public void decreaseSeverityNonMultiBranchFeatureBranch() throws Exception {
-
-        assertFalse(mapper.decreaseSeverity(jobInfo(false, false)));
-    }
-
-    @Test
-    public void decreaseSeverityNonMultiBranchMaster() throws Exception {
-
-        assertFalse(mapper.decreaseSeverity(jobInfo(false, true)));
-    }
-
-    @Test
-    public void identifyStatusFailedTestCountRegularSeverity() throws Exception {
-
-        assertEquals(State.YELLOW, mapper.identifyStatus(buildInfo(), 1, jobInfo(false, true)));
-    }
-
-    @Test
-    public void identifyStatusFailedTestCountDecreasedSeverity() throws Exception {
-
-        assertEquals(State.GREY, mapper.identifyStatus(buildInfo(), 1, jobInfo(true, false)));
+    public void identifyStatusFailedTestCountStaleBranch() throws Exception {
+        assertEquals(State.GREY, mapper.identifyStatus(buildInfo(), 1, jobInfo(true, false, build(true))));
     }
 
     @Test
     public void identifyStatusGreenIfBuildInfoIsNull() throws Exception {
-
-        assertEquals(State.GREEN, mapper.identifyStatus(null, 1, jobInfo(true, false)));
+        assertEquals(State.GREEN, mapper.identifyStatus(null, 1, jobInfo(true, false, build(false))));
     }
 
-    private JenkinsJobInfo jobInfo(boolean multibranch, boolean master) {
+    @Test
+    public void identifyStatusWithNullBuildInfoRegular() {
+        assertEquals(State.RED, mapper.identifyStatus(buildInfo(), 0, jobInfo(false, true, build(false))));
+    }
 
+    @Test
+    public void identifyStatusWithNullBuildInfoActiveBranch() {
+        assertEquals(State.GREY, mapper.identifyStatus(buildInfo(), 0, jobInfo(true, false, build(false))));
+    }
+
+    @Test
+    public void identifyStatusWithNullBuildInfoStaleBranch() {
+        assertEquals(State.YELLOW, mapper.identifyStatus(buildInfo(), 0, jobInfo(true, false, build(true))));
+    }
+
+    private Build build(boolean twoWeeksOld) {
+        final Build build = new Build();
+        final Long timestamp = twoWeeksOld ? System.currentTimeMillis() / 1000 - (2 * 7 * 24 * 3600) : System.currentTimeMillis() / 1000;
+        build.setTimestamp(timestamp);
+        return build;
+    }
+
+    private JenkinsJobInfo jobInfo(boolean multibranch, boolean master, Build lastBuild) {
         final JenkinsJobInfo jobInfo = new JenkinsJobInfo();
         jobInfo.setName(master ? "master" : "featureABC");
         jobInfo.setProperties(Collections.singletonList(new Property(multibranch ? Property.MULTIBRANCH_CLASS : "SomeOtherClass")));
+        jobInfo.setLastBuild(lastBuild);
         return jobInfo;
     }
 
