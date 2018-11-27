38bc7ea14f6e5e6843d86b16364a73c77abd659e
Henning Gross <henning@upday.com>
2018-11-27 14:55:13 +0100

    - Use timestamp on right object (#118)
    - Decrease severity on aborted jobs, too

--- dash-core/src/test/java/de/axelspringer/ideas/tools/dash/business/jenkins/executor/JenkinsJobToStateMapperTest.java
+++ dash-core/src/test/java/de/axelspringer/ideas/tools/dash/business/jenkins/executor/JenkinsJobToStateMapperTest.java
@@ -17,43 +17,41 @@ public class JenkinsJobToStateMapperTest {
 
     @Test
     public void identifyStatusFailedTestCountRegular() throws Exception {
-        assertEquals(State.YELLOW, mapper.identifyStatus(buildInfo(), 1, jobInfo(false, true, build(false))));
+        assertEquals(State.YELLOW, mapper.identifyStatus(buildInfo(false), 1, jobInfo(false, true, build())));
     }
 
     @Test
     public void identifyStatusFailedTestCountActiveBranch() throws Exception {
-        assertEquals(State.GREEN, mapper.identifyStatus(buildInfo(), 1, jobInfo(true, false, build(false))));
+        assertEquals(State.GREEN, mapper.identifyStatus(buildInfo(false), 1, jobInfo(true, false, build())));
     }
 
     @Test
     public void identifyStatusFailedTestCountStaleBranch() throws Exception {
-        assertEquals(State.GREY, mapper.identifyStatus(buildInfo(), 1, jobInfo(true, false, build(true))));
+        assertEquals(State.GREY, mapper.identifyStatus(buildInfo(true), 1, jobInfo(true, false, build())));
     }
 
     @Test
     public void identifyStatusGreenIfBuildInfoIsNull() throws Exception {
-        assertEquals(State.GREEN, mapper.identifyStatus(null, 1, jobInfo(true, false, build(false))));
+        assertEquals(State.GREEN, mapper.identifyStatus(null, 1, jobInfo(true, false, build())));
     }
 
     @Test
     public void identifyStatusWithNullBuildInfoRegular() {
-        assertEquals(State.RED, mapper.identifyStatus(buildInfo(), 0, jobInfo(false, true, build(false))));
+        assertEquals(State.RED, mapper.identifyStatus(buildInfo(false), 0, jobInfo(false, true, build())));
     }
 
     @Test
     public void identifyStatusWithNullBuildInfoActiveBranch() {
-        assertEquals(State.GREY, mapper.identifyStatus(buildInfo(), 0, jobInfo(true, false, build(false))));
+        assertEquals(State.GREY, mapper.identifyStatus(buildInfo(false), 0, jobInfo(true, false, build())));
     }
 
     @Test
     public void identifyStatusWithNullBuildInfoStaleBranch() {
-        assertEquals(State.YELLOW, mapper.identifyStatus(buildInfo(), 0, jobInfo(true, false, build(true))));
+        assertEquals(State.YELLOW, mapper.identifyStatus(buildInfo(true), 0, jobInfo(true, false, build())));
     }
 
-    private Build build(boolean twoWeeksOld) {
+    private Build build() {
         final Build build = new Build();
-        long twoWeeksAgo = System.currentTimeMillis() - (14 * 24 * 3600 * 1000);
-        build.setTimestamp(twoWeeksOld ? twoWeeksAgo : System.currentTimeMillis());
         return build;
     }
 
@@ -65,7 +63,11 @@ public class JenkinsJobToStateMapperTest {
         return jobInfo;
     }
 
-    private JenkinsBuildInfo buildInfo() {
-        return new JenkinsBuildInfo();
+    private JenkinsBuildInfo buildInfo(boolean twoWeeksOld) {
+
+        final JenkinsBuildInfo jenkinsBuildInfo = new JenkinsBuildInfo();
+        long twoWeeksAgo = System.currentTimeMillis() - (14 * 24 * 3600 * 1000);
+        jenkinsBuildInfo.setTimestamp(twoWeeksOld ? twoWeeksAgo : System.currentTimeMillis());
+        return jenkinsBuildInfo;
     }
 }
